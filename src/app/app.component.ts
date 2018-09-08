63eb058330048a1fbbd5ecaf80d86bcea90edb85
Indresh <ips.indresh@gmail.com>
2018-09-08 13:04:46 +0530

    added header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
+// component decorator
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  selector: 'app-root', // tag or name
+  templateUrl: './app.component.html',  // html template
+  styleUrls: ['./app.component.css'] // own css optional
+}) 
 export class AppComponent {
   title = 'nglearn10';
 }
---
+++ src/app/header.component.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:"app-header",
+    template:`
+    <header>
+        <h1>{{ title }}</h1>
+
+        <div> {{ description }}</div>
+    </header>
+    `
+})
+export class  HeaderComponent {
+ title = "Angular Application";
+ description = "This is description";
+ constructor(){
+     setTimeout(() => {
+            this.title = "Changed the title";
+     }, 5000);
+ }
+}
