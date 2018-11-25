ce06ced8f3e1e2860ee8f659cb5fd9873d181bac
Indresh <ips.indresh@gmail.com>
2018-11-25 14:36:42 +0530

    template driven form (Login)

--- src/app/containers/login/login.component.ts
+++ src/app/containers/login/login.component.ts
@@ -5,11 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent  {
+
+  username;
+  password;
 
   constructor() { }
 
-  ngOnInit() {
+  doLogin(frm){
+    console.log(frm);
   }
+
+
 
 }
