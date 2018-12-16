a0a6e13bb21c7bb8a09d9b13b4c83681f1999a54
Indresh <ips.indresh@gmail.com>
2018-12-16 14:17:00 +0530

    user registration

--- src/app-modules/student/containers/stu-registration/stu-registration.component.ts
+++ src/app-modules/student/containers/stu-registration/stu-registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'stu-registration',
@@ -7,9 +8,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StuRegistrationComponent implements OnInit {
 
+  regForm: FormGroup;
+  bloodGroupOpts = [{ label: 'O+', id: 1 }, { label: 'B+', id: 2 }, { label: 'AB', id: 3 }, { label: 'AB-', id: 4 }];
+
   constructor() { }
 
   ngOnInit() {
+    this.prepareForm();
+  }
+
+  save() {
+    console.log(this.regForm.value);
+  }
+  rawData() {
+    console.log(this.regForm.getRawValue());
+  }
+
+  prepareForm() {
+    this.regForm = new FormGroup({
+      firstname: new FormControl(null, [Validators.required, Validators.minLength(2), Validators.maxLength(20)]),
+      middlename: new FormControl(),
+      lastname: new FormControl(),
+      gender: new FormControl('Boy', [Validators.required])
+    });
+  }
+
+
+  fillTheForm() {
+    //always take value for each control
+    this.regForm.setValue({
+      firstname: 'indresh',
+      middlename: 'pratap',
+      lastname: 'singh',
+      gender: 'Boy'
+    });
+  }
+
+  patchTheForm() {
+    //always take value for each control
+    this.regForm.patchValue({
+      firstname: 'indresh',
+      middlename: 'pratap',
+      lastname: 'singh'
+     
+    });
+  }
+
+  addBloodGroupCtrl() {
+    //dynamic add new controls to existing form
+    // here null is default value
+    this.regForm.addControl('bloodGroup', new FormControl(null));
   }
 
 }
--- src/app-modules/ui-kit/ui-kit.module.ts
+++ src/app-modules/ui-kit/ui-kit.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { pipes } from './pipes';
 import {uiDirectives} from './directives';
 @NgModule({
     declarations: [...pipes,...uiDirectives],
-    imports: [ CommonModule,FormsModule ],
-    exports: [...pipes,...uiDirectives,CommonModule,FormsModule],
+    imports: [ CommonModule,FormsModule,ReactiveFormsModule ],
+    exports: [...pipes,...uiDirectives,CommonModule,FormsModule,ReactiveFormsModule],
     providers: [],
 })
 export class UiKitModule {}
